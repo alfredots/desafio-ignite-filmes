e9abbead95079378deda56c09ef5cbefd52c5be7
alfredo <alfredtito97@gmail.com>
2021-07-13 11:12:43 -0300

    feat(Content/Sidebar): separação dos componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,25 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App"
+import { MovieCard } from './MovieCard'
+
+type ContentProps = {
+  title: string
+  movies: MovieProps[]
+}
+
+export function Content({ title, movies }:ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> { title }</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
